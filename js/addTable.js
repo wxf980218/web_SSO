4b3c465ea31fd52e70fe7c803b987da3aa492d43
吴俊 <707957068@qq.com>
2019-08-01 16:37:42 +0800

    考评管理表格功能完成

--- js/addTable.js
+++ js/addTable.js
@@ -1,5 +1,12 @@
 //appraisalManagement的数据自动填入
 $(function () {
+    //定义全局结点,结点在JSON中的下标
+    var treeNodeInfo,index;
+    //根据键获取值
+    var temp = localStorage.getItem('table');
+    //变为JSON
+    var tempJson = JSON.parse(temp);
+
     var curMenu = null, zTree_Menu = null;
     var setting = {
         view: {
@@ -45,12 +52,18 @@ $(function () {
         var btn = $("#addBtn_"+treeNode.tId);
         if (btn) btn.bind("click", function(){
             $("#modalAdd").modal({});        //调用添加模态框
-            $('.closeModal,#modalAddconfirm').unbind('click').bind('click',function (event) {
+            $('.closeModal,#modalAddconfirm,.close').unbind('click').bind('click',function (event) {
                 if(event.target.id == 'modalAddconfirm'){
                     var zTree = $.fn.zTree.getZTreeObj("treeDemo");
                     zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:$('#addInput').val()});
                     newCount++;
-                }
+                    $('#modalAdd #modalBody').empty();
+                }else if(event.target.class == 'closeModal'){
+                    $('#modalAdd #modalBody').empty();
+                }else if(event.target.class == 'close')
+                    $('#modalAdd #modalBody').empty();
+                $('#modalAdd').modal('toggle');
+
             });
             return false;
         });
@@ -74,11 +87,11 @@ $(function () {
         $('.close,.closeModal,#confirm').unbind('click').bind('click',function (event) {
             if(event.target.id == 'confirm'){
                 var zTree = $.fn.zTree.getZTreeObj(treeId);
-                zTree.removeNode(treeNode)
+                zTree.removeNode(treeNode);
             }else if(event.target.class == 'close'){
-                $('#modalBody').empty();  //点×时将模态框内容置空
+                $('#modalDel #modalBody').empty();  //点×时将模态框内容置空
             }
-            $('#modalBody').empty();                /*取消、确认模态框时将模态框内容置空*/
+            $('#modalDel #modalBody').empty();  //点取消时将模态框内容置空
             $('#modalDel').modal('toggle');
         });
         return false;
@@ -96,60 +109,93 @@ $(function () {
             ,done:function (value,date) { //控件选择完毕的回调
                 year = JSON.stringify(date.year); //获取年
                 month = JSON.stringify(date.month); //获取月
+                // 判断结点
+                 if(treeNodeInfo!=null&&treeNodeInfo.level==2){
+                     //判断年月
+                     //  console.log(tempJson[index]["趋势表"]["日期"][0]["月"]);
+                     //   console.log(tempJson[index]["趋势表"]["日期"][1]["月"]);
+                     if(treeNodeInfo.hasOwnProperty("index")){
+                         for(var i=0;i<tempJson[index]["趋势表"]["日期"].length;i++){
+                             if(year==tempJson[index]["趋势表"]["日期"][i]["年"]&&
+                                     month==tempJson[index]["趋势表"]["日期"][i]["月"]){
+                                     //更新表单信息
 
-                // //    判断是否存在变量x
-                // if(localStorage.hasOwnProperty(tes)){
-                //     //若存在，调出表
-                //     console.log("已存在");
-                // }else{
-                //     //若不存在，写入
-                //     localStorage.setItem(tes,tes);
-                //     //表置空
-                //
-                //     console.log("不存在");
-                // }
-                // console.log(localStorage);
+                                     $("#11z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["1"].直接领导考核);
+                                     $("#11j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["1"].分管领导考核);
+                                     $("#11bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["1"].备注);
+                                     $("#12z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["2"].直接领导考核);
+                                     $("#12j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["2"].分管领导考核);
+                                     $("#12bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["2"].备注);
+                                     $("#13z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["3"].直接领导考核);
+                                     $("#13j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["3"].分管领导考核);
+                                     $("#13bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["3"].备注);
+                                     $("#14z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["4"].直接领导考核);
+                                     $("#14j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["4"].分管领导考核);
+                                     $("#14bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["4"].备注);
+                                     $("#15z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["合计"].直接领导考核);
+                                     $("#15j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["合计"].分管领导考核);
+                                     $("#15bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["合计"].备注);
+                                     return;
+                             }
+                         }
+                         //year和month在数组中没有找到
+                         alert(year+'年'+month+'月'+"还没有考评表！");
+                         $('input').val("");//置空
+                     }  //没有index
+                     else{
+                         // alert("还没有index！");
+                         $('input').val("");//置空
+                     }
+                 }
+
+                 else{
+                     $('input').val("");//置空
+                 }
             }
         });
     });
 
-
-    //定义全局结点,结点在JSON中的下标
-    var treeNodeInfo,index;
-    //根据键获取值
-    var temp = localStorage.getItem('table');
-    //变为JSON
-    var tempJson = JSON.parse(temp);
-
     //点击人员后将趋势表信息渲染到右边表格中
     function ztreeOnclick(event,treeId, treeNode){
         treeNodeInfo = treeNode;
-        if(treeNode.level == 2){
-            //判断日期(测试日期，2019年12月)
-            if(year==treeNode.趋势表["日期"]["年"]&&
-              month==treeNode.趋势表["日期"]["月"]){
-                //table表填内容
-                index = treeNodeInfo.index;  //获取结点在JSON数组中的下表
-                $("#11z").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["1"].直接领导考核);
-                $("#11j").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["1"].分管领导考核);
-                $("#11bei").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["1"].备注);
-                $("#12z").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["2"].直接领导考核);
-                $("#12j").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["2"].分管领导考核);
-                $("#12bei").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["2"].备注);
-                $("#13z").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["3"].直接领导考核);
-                $("#13j").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["3"].分管领导考核);
-                $("#13bei").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["3"].备注);
-                $("#14z").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["4"].直接领导考核);
-                $("#14j").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["4"].分管领导考核);
-                $("#14bei").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["4"].备注);
-                $("#15z").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["合计"].直接领导考核);
-                $("#15j").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["合计"].分管领导考核);
-                $("#15bei").val(tempJson[index]["趋势表"]["日期"]["表"]["个人素质"]["合计"].备注);
+        index = treeNodeInfo.index;  //点击结点获取结点在JSON数组中的下表
+        // console.log(year+month);
+        if(year!=null&&month!=null){
+            $('.layui-input').attr("placeholder",year+'-'+month);
+            if(treeNode.level == 2&&treeNode.hasOwnProperty("趋势表")){
+                //判断日期(测试日期，2019年12月)
+                for(var i=0;i<tempJson[index]["趋势表"]["日期"].length;i++){
+                    if(year==treeNode.趋势表["日期"][i]["年"]&&
+                        month==treeNode.趋势表["日期"][i]["月"]){
+                        //table表填内容
+                        $("#11z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["1"].直接领导考核);
+                        $("#11j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["1"].分管领导考核);
+                        $("#11bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["1"].备注);
+                        $("#12z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["2"].直接领导考核);
+                        $("#12j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["2"].分管领导考核);
+                        $("#12bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["2"].备注);
+                        $("#13z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["3"].直接领导考核);
+                        $("#13j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["3"].分管领导考核);
+                        $("#13bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["3"].备注);
+                        $("#14z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["4"].直接领导考核);
+                        $("#14j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["4"].分管领导考核);
+                        $("#14bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["4"].备注);
+                        $("#15z").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["合计"].直接领导考核);
+                        $("#15j").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["合计"].分管领导考核);
+                        $("#15bei").val(tempJson[index]["趋势表"]["日期"][i]["表"]["个人素质"]["合计"].备注);
+                        return;
+                    }else{
+                        continue;
+                    }
+                }
+                //没有找到
+                alert(year+'年'+month+'月'+"还没有考评表！");
+                $('input').val("");//置空
             }else{
+                // alert("还没有index！");
                 $('input').val("");//置空
             }
-
-        }else{
+        }else {
             $('input').val("");//置空
         }
     }
